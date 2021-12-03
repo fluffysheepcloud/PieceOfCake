9bff13d33b89145b5d5cf317013834af69733e04
BlackKnife <lding1003@gmail.com>
2021-12-02 19:15:40 -0800

    Data API
    API for requesting data

--- back-end/src/test/java/DataSourceTest.java
+++ back-end/src/test/java/DataSourceTest.java
@@ -1,4 +1,4 @@
-import com.cookie.entity.User;
+import com.demo.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
