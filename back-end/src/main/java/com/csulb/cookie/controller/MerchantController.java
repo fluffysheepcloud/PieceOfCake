b24029056dd02a0baff03eac2eb1240ba08ce331
Sydney Kao <sydney_kao@sbcglobal.net>
2022-01-26 18:44:32 -0800

    Update MerchantController.java
    May need repository class for merchant (and customer)

--- back-end/src/main/java/com/csulb/cookie/controller/MerchantController.java
+++ back-end/src/main/java/com/csulb/cookie/controller/MerchantController.java
@@ -6,15 +6,17 @@ package com.csulb.cookie.controller;
 
 import com.csulb.cookie.domain.Merchant;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// I think we will need a repository class... Will add later
+
 @Controller
 public class MerchantController {
 
-    @RequestMapping("/merchant")
-    @ResponseBody
+    @PostMapping("/addMerchant")
     public Merchant merchant(@RequestParam int id,
                              @RequestParam String username,
                              @RequestParam String shop_name,
