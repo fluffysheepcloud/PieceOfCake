3c980790a9bd50c5b13f8f671fdbe6ae53d79e17
Sydney Kao <sydney_kao@sbcglobal.net>
2022-02-11 23:52:32 -0800

    modifications in format

--- front-end/lib/pages/profile/account_registration.dart
+++ front-end/lib/pages/profile/account_registration.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'customer_registration.dart';
 import 'MerchantRegistrationPage1.dart';
 
-
 class AccountRegistrationScreen extends StatelessWidget {
 
   var arguments;
