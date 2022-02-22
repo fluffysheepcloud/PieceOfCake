f31d49eaea8cb2fa831b3fb96c5663f82044284a
SabrinaLiu999 <60949285+SabrinaLiu999@users.noreply.github.com>
2022-02-21 21:54:07 -0800

    Merge remote-tracking branch 'origin/main'

--- front-end/lib/pages/shopping_cart/shopping_cart_card.dart
+++ front-end/lib/pages/shopping_cart/shopping_cart_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-class ShoppingCartCard extends StatelessWidget {
+class ShoppingCartCard extends StatefulWidget {
   String orderCakeID;
   String imageURL;
   double cakeSize;
@@ -12,12 +11,23 @@ class ShoppingCartCard extends StatelessWidget {
   int dataIndex;
 
 
+
   ShoppingCartCard({required this.orderCakeID, required this.imageURL,
     required this.cakeSize, required this.baseColor, required this.baseFlavor,
     required this.frostingColor, required this.frostingFlavor, required this.price,
     required this.dataIndex, Key? key})
       : super(key: key);
 
+  //growable list (make sure dataIndex and quantity Index are the same)
+  List  quantityList = List .filled(100, 1, growable: true);
+
+
+
+  @override
+  _ShoppingCartCardState createState() => _ShoppingCartCardState();
+}
+
+class _ShoppingCartCardState extends State<ShoppingCartCard> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -27,7 +37,7 @@ class ShoppingCartCard extends StatelessWidget {
               .of(context)
               .size
               .width * 0.85,
-          height: 160.0,
+          height: 220.0,
           child: Card(
             elevation: 5,
             color: Colors.grey[200],
@@ -39,6 +49,7 @@ class ShoppingCartCard extends StatelessWidget {
                 _pictureAndInfo(),
                 Divider(thickness: 2.0, color: Colors.brown[200],),
                 _price(),
+                _quantity(widget.dataIndex),
               ],
             ),),
         )
@@ -52,7 +63,7 @@ class ShoppingCartCard extends StatelessWidget {
             padding: EdgeInsets.only(
                 left: 8, top: 5),
             child: Text(
-                "Cake ID: ${orderCakeID}")
+                "Cake ID: ${widget.orderCakeID}")
         ),
       ],
     );
@@ -69,7 +80,7 @@ class ShoppingCartCard extends StatelessWidget {
               width:120,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(8.0),
-                child: Image.asset(imageURL, fit: BoxFit.cover),
+                child: Image.asset(widget.imageURL, fit: BoxFit.cover),
               )
           ),
         ),
@@ -81,8 +92,8 @@ class ShoppingCartCard extends StatelessWidget {
                 height: 80,
                 width: 100,
                 child: Text(
-                    "Cake Size: ${cakeSize} '', Base color: ${baseColor}, Base flavor: ${baseFlavor},"
-                        " Frosting Color:${frostingColor}, Frosting Flavor:${frostingFlavor}",
+                    "Cake Size: ${widget.cakeSize} '', Base color: ${widget.baseColor}, Base flavor: ${widget.baseFlavor},"
+                        " Frosting Color:${widget.frostingColor}, Frosting Flavor:${widget.frostingFlavor}",
 
                     overflow: TextOverflow.ellipsis,
                     maxLines: 5
@@ -103,13 +114,59 @@ class ShoppingCartCard extends StatelessWidget {
             padding: EdgeInsets.only(
                 left: 8, top: 5),
             child: Text(
-                "Unit Price: ${price},"
+                "Unit Price: ${widget.price},"
             )
         ),
       ],
     );
   }
+  Widget _quantity(int dataIndex) {
+    return Card(
+      elevation: 1.0,
+      child: Padding(
+        padding: const EdgeInsets.only(
+            left: 150, top: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            _decrementButton(dataIndex),
+            Text(
+              '${ widget.quantityList[dataIndex]}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            _incrementButton(dataIndex),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _incrementButton(int index) {
+    return FloatingActionButton(
+      child: Icon(Icons.add, color: Colors.black87),
+      backgroundColor: Colors.white,
+      onPressed: () {
+        setState(() {
+          widget.quantityList[index]++;
+        });
+      },
+    );
+  }
+
+  Widget _decrementButton(int index) {
+    return FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            widget.quantityList[index]--;
+            if (widget.quantityList[index] <= 0)
+              {
+                widget.quantityList[index] = 0;
+              }
+          });
+        },
+        child: new Icon(Icons.remove, color: Colors.black87),
+        backgroundColor: Colors.white);
+  }
+
+
 
 }
-
-
