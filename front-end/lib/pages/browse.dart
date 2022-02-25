15c6f469ea5934cbb4df63d9c1b024ce63e1902a
Sarah Lum <petrichormist581@gmail.com>
2022-02-24 19:03:23 -0800

    working on search function

--- front-end/lib/pages/browse.dart
+++ front-end/lib/pages/browse.dart
@@ -11,16 +11,36 @@ class Browse extends StatefulWidget{
 
   @override
   _BrowseState createState() => _BrowseState();
-
 }
 
 class _BrowseState extends State<Browse>{
+  final List _items = [];
+  readJson() async {
+    var m1 = await getMerchantInfoById(1);
+    var m2 = await getMerchantInfoById(2);
+    var m3 = await getMerchantInfoById(3);
+
+    //add all results into a initial list
+    setState(() {
+      _items.add(m1["data"]);
+      _items.add(m2["data"]);
+      _items.add(m3["data"]);
+
+    });
+  }
+  @override
+  void initState(){
+
+  }
+
+
   Icon customIcon = const Icon(Icons.search);
   Widget customSearchBar = const Text('Search');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
         children: [
           //searchbar
           Container(
@@ -32,9 +52,7 @@ class _BrowseState extends State<Browse>{
                       labelText: 'Search',
                       //just make regular icon
                       suffixIcon: IconButton(
-
                         icon: customIcon, onPressed: () {  },
-
                       )
                   ),
                   //if textfield tapped then turn icon into cancel, if icon button tapped and it is a cancel, then cancel transaction
@@ -60,7 +78,10 @@ class _BrowseState extends State<Browse>{
                   }
               )
           ),
+          //end searchbar
+
         ],
+      )
       )
     );
   }
