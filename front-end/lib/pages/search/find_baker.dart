9498f9c593b2767b2e725c411501e24b51460a2e
Sarah Lum <petrichormist581@gmail.com>
2022-04-07 16:43:11 -0700

    Update find_baker.dart
    text boxes for find baker

--- front-end/lib/pages/search/find_baker.dart
+++ front-end/lib/pages/search/find_baker.dart
@@ -18,6 +18,51 @@ class FindBaker extends StatefulWidget{
 //read in json array
 //copy array into _items
 //if query not empty and _items.contains(query) --> add
+Widget _textBoxStyle({required String title}){
+  return SizedBox(
+    width: 400,
+    child:Row(
+        children:<Widget> [
+         Container(
+             height: 52,
+             width: 100,
+             decoration: BoxDecoration(
+               color: Colors.brown[200],
+               borderRadius: BorderRadius.only(
+                   topLeft: Radius.circular(10.0),
+                   bottomLeft: Radius.circular(10.0)
+               )
+             ),
+
+              child: Align(
+               alignment: Alignment.center,
+               child: Text(title),
+              )
+               ),
+          Expanded(
+            child: Container(
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.brown),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10.0),
+                            bottomRight: Radius.circular(10.0))
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.amber))
+                ),
+              ),
+          )
+          )
+        ]
+    ),
+  );
+
+}
+
 
 class _FindBakerState extends State<FindBaker>{
   List  _items = [];
@@ -65,13 +110,30 @@ class _FindBakerState extends State<FindBaker>{
             child: Column(
 
               children:<Widget>[
-                SizedBox(
-                width: 42.0,
-                height: 42.0,
-                child: const DecoratedBox(
-                decoration: const BoxDecoration(
-                color: Colors.red
-                ))),
+                Container(
+                  width: 400.0,
+                  height: 400.0,
+
+                  decoration: BoxDecoration(
+                    color: Colors.brown[100],
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          bottomLeft: Radius.circular(10.0)
+                      )
+                  ),
+                  margin: const EdgeInsets.all(10.0) ,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _textBoxStyle(title: "location"),
+                      SizedBox(
+                        height: 20.0,
+                        width: 20.0,
+                      ),
+                      _textBoxStyle(title: "zipcode")
+                    ],
+                  ),
+                ),
 
                 results.isNotEmpty
                     ? ListView.builder(
@@ -100,3 +162,4 @@ class _FindBakerState extends State<FindBaker>{
     );
   }
 }
+
