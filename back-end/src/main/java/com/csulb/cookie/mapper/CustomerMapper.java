9baeb20c6f5e46aeefea1a8c25ebf3786061704a
BlackKnife <lding1003@gmail.com>
2022-04-07 16:26:16 -0700

    Reviewed & Unreviewed Orders

--- back-end/src/main/java/com/csulb/cookie/mapper/CustomerMapper.java
+++ back-end/src/main/java/com/csulb/cookie/mapper/CustomerMapper.java
@@ -1,14 +1,17 @@
 package com.csulb.cookie.mapper;
 
+import com.csulb.cookie.bean.Review;
 import com.csulb.cookie.domain.Customer;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.csulb.cookie.domain.Merchant;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Map;
+import java.util.List;
 
 @Mapper
 public interface CustomerMapper extends BaseMapper<Customer> {
+
+    List<Review> getUnreviewedOrders(Integer id);
+    List<Review> getReviewedOrders(Integer id);
 
 }
 
