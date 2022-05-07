5ca167a8d8833b2b1143ae1b22c8d02da8a0c2e8
BlackKnife <lding1003@gmail.com>
2022-05-06 23:31:09 -0700

    Add prebuild cake for merchant

---
+++ back-end/src/main/java/com/csulb/cookie/mapper/FavoriteMapper.java
@@ -0,0 +1,13 @@
+package com.csulb.cookie.mapper;
+
+import com.csulb.cookie.domain.Favorite;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+public interface FavoriteMapper extends BaseMapper<Favorite> {
+
+}
+
+
+
+
